872bd4b419319f8d6e659f69c88689ae7a13c179
mauricio094 <mau_santos09@hotmail.com>
2017-08-13 02:20:51 -0300

    Criar estrutura de pastas do projeto

---
+++ app/src/main/java/com/nucounts/NuCountsApplication.java
@@ -0,0 +1,12 @@
+package br.com.nucounts.NuCounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuCountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuCountsApplication.class, args);
+	}
+}
