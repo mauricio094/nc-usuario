d486605601729dea018ad59d1907008fae465e9b
Mauricio dos Santos <mau_santos09@hotmail.com>
2017-08-24 02:33:56 -0300

    criar usecase de salvar usuario

--- app/src/main/java/com/ncusuario/usecases/SaveUsuario.java
+++ app/src/main/java/com/ncusuario/usecases/SaveUsuario.java
@@ -1,4 +1,21 @@
 package com.ncusuario.usecases;
 
+import com.ncusuario.domains.Usuario;
+import com.ncusuario.gateways.UsuarioGateway;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@AllArgsConstructor
 public class SaveUsuario {
+    private UsuarioGateway usuarioGateway;
+
+    @Async
+    public void execute(final Usuario usuario) {
+        log.info("Saving usuario:{}", usuario);
+        usuarioGateway.save(usuario);
+    }
 }
