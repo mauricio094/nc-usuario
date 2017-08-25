ad880c7e38560e0e69e5417142680e6ba929af3e
Mauricio dos Santos <mau_santos09@hotmail.com>
2017-08-25 00:34:36 -0300

    Adicionar metodo findBySku na controller

--- app/src/main/java/com/ncusuario/gateways/http/UsuarioController.java
+++ app/src/main/java/com/ncusuario/gateways/http/UsuarioController.java
@@ -1,9 +1,9 @@
 package com.ncusuario.gateways.http;
 
 import com.ncusuario.gateways.http.resources.request.UsuarioRequest;
+import com.ncusuario.gateways.http.resources.response.UsuarioResponse;
 import com.ncusuario.usecases.FindUsuario;
 import com.ncusuario.usecases.SaveUsuario;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -12,10 +12,12 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+import static java.util.Optional.ofNullable;
+
+
 @Slf4j
 @RestController
 @RequestMapping("api/v1")
-@AllArgsConstructor
 public class UsuarioController {
 
     private FindUsuario findUsuario;
@@ -31,6 +33,12 @@ public class UsuarioController {
     public ResponseEntity.BodyBuilder saveUser(@RequestBody @Valid final UsuarioRequest usuarioRequest) {
         saveUsuario.execute(usuarioRequest.toDomain());
         return ResponseEntity.ok();
+    }
+
+    @RequestMapping(value = "/user{email}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UsuarioResponse> getUsuarioByEmail(@PathVariable final String email) {
+        return ofNullable(this.findUsuario.execute(email)).map(usuario -> ResponseEntity.ok(new UsuarioResponse(usuario)))
+                .orElse(ResponseEntity.notFound().build());
     }
 
 }
